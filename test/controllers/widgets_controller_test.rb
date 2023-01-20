78a54b9b14e0105a7b3bad82f3b0b0f7aa63dac4
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-01-20 02:33:32 -0800

    discard test

--- test/controllers/widgets_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class WidgetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get widgets_show_url
-    assert_response :success
-  end
-end
