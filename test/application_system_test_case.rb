91796ee27494be2938272221f6b46acb7eab2684
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-01-19 19:20:11 -0800

    inclusion of `with_clues`

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,8 @@
 require "test_helper"
+require "support/with_clues"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400] # this is the default
+  include TestSupport::WithClues
   driven_by :rack_test
 end
