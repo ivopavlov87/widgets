2d4fcc478ecfc5cc3e224b6dea6cab18f0d9aa6d
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-01-22 17:24:02 -0800

    seeds for dev env

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+if !Rails.env.development?
+  puts "[ db/seeds.rb ] Seed data is for develpment only, " +
+       "not #{Rails.env}"
+
+  exit 0
+end
+
+require "factory_bot"
+
+Widget.destroy_all
+Manufacturer.destroy_all
+Address.destroy_all
+WidgetStatus.destroy_all
+
+puts "[ db/seeds.rb ] Creating development data..."
+FactoryBot.create(:widget_status, name: "Fresh")
+10.times do
+  FactoryBot.create(:manufacturer)
+end
+cyberdyne = FactoryBot.create(:manufacturer,
+                              name: "Cyberdyne Systems")
+FactoryBot.create(:widget, name: "Stembolt",
+                           manufacturer: cyberdyne)
+puts "[ db/seeds.rb ] Done"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
