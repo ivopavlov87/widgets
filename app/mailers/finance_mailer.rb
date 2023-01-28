8f598a34184ba8605e604fc0f8a6134fe22c5340
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-01-28 15:21:19 -0800

    clean up and adding `:application` helper

--- app/mailers/finance_mailer.rb
+++ app/mailers/finance_mailer.rb
@@ -1,10 +1,5 @@
 class FinanceMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.finance_mailer.high_priced_widget.subject
-  #
+  helper :application  
   def high_priced_widget(widget)
     @widget = widget
 
