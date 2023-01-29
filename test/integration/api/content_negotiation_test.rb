bb40a451cb8c16f8ff212adcab2917eb8de24bd0
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-01-29 13:34:12 -0800

    API related tests

---
+++ test/integration/api/content_negotiation_test.rb
@@ -0,0 +1,35 @@
+require "test_helper.rb"
+
+class Api::ContentNegotiationTest < ActionDispatch::IntegrationTest
+  test "a non-JSON Accet header gets a 406" do
+    api_key = FactoryBot.create(:api_key)
+    authorization = ActionController::
+                      HttpAuthentication::
+                        Token.encode_credentials(api_key.key)
+    
+    widget = FactoryBot.create(:widget)
+
+    get api_v1_widget_path(widget),
+      headers: {
+        "Accept" => "text/plain",
+        "Authorization" => authorization
+      }
+
+    assert_response 406
+  end
+  test "no Accept header gets a 406" do
+    api_key = FactoryBot.create(:api_key)
+    authorization = ActionController::
+                      HttpAuthentication::
+                        Token.encode_credentials(api_key.key)
+    
+    widget = FactoryBot.create(:widget)
+
+    get api_v1_widget_path(widget),
+      headers: {
+        "Authorization" => authorization
+      }
+
+    assert_response 406
+  end
+end
