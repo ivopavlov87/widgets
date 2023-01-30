a3c93cb5673a62ce07a1f88afb2b5ad6854a3f81
Ivo Pavlov <pavlov.ivo@gmail.com>
2023-01-29 16:15:14 -0800

    useful server logs logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_request_id_in_thread_local
+
+  def set_request_id_in_thread_local
+    Thread.current.thread_variable_set(
+      "request_id", request.request_id
+    )
+  end
+
+  def append_info_to_payload(payload)
+    super
+    payload[:request_id] = request.request_id
+  end
 end
